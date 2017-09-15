151b166f0858506bcc1883df7e28fcee7536ea87
Pep3 <Mppimp@users.noreply.github.com>
2017-09-15 12:09:16 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  destroy_the_world
+//
+//  Created by Pepe on 9/14/17.
+//  Copyright © 2017 Pepe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello, World!");
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
